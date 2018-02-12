a31310d64115a21d249f0d63be2bbd305016e870
AsadShahzad1 <35929059+AsadShahzad1@users.noreply.github.com>
2018-02-13 01:44:20 +0500

    Update Task 2.cs

--- Lab 4/Task 2.cs
+++ Lab 4/Task 2.cs
@@ -1,1 +1,34 @@
+        using System;
+        using System.Collections.Generic;
+        using System.Linq;
+        using System.Text;
+        using System.Threading.Tasks;
 
+        namespace ConsoleApplication1
+        {
+
+            public static class Factor
+            {
+                public static long FactorialUsingFor(this long factorialNumber)
+                {
+                    int product = 1;
+                    for (int i = 1; i <= factorialNumber; i++)
+                    {
+                        product = product * i;
+
+                    }
+                    return product;
+                }
+            }
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    long number = 5;
+        long result = number.FactorialUsingFor();
+        Console.WriteLine(result);
+           
+                    Console.ReadLine();
+                }
+            }
+                }
