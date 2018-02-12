e165a83f049333540008a29cfd2e98337cd4e909
AsadShahzad1 <35929059+AsadShahzad1@users.noreply.github.com>
2018-02-13 01:12:59 +0500

    Add files via upload

---
+++ Lab 5/TASK 1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication15
+{
+    class information
+    {
+        string name;
+        string location;
+        int model;
+        int speed;
+        int weight;
+
+
+        public information(string name, string location)
+        {
+            this.name = name;
+            this.location = location;
+            Console.WriteLine("Bicycle specificiation");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("location  " + location);
+        }
+
+        public information(string name, int model, int speed)
+        {
+            Console.WriteLine("Car specification");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("model  " + model);
+            Console.WriteLine("speed  " + speed);
+        }
+
+        public information(string name, int model, int speed, int weight)
+        {
+            Console.WriteLine("lorry specification");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("model  " + model);
+            Console.WriteLine("speed  " + speed);
+            Console.WriteLine("weight " + weight);
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("*****************");
+            information bicycle = new information("BMX", "Pakistan\n");
+            Console.WriteLine("*****************");
+            information car = new information("Ferrari", 9821, 1000);
+            Console.WriteLine("*****************");
+            information lorry = new information("Honda", 2314, 2313, 21312);
+            Console.WriteLine("*****************");
+            Console.ReadLine();
+
+        }
+    }
+}
