e39e788a1b269a05727ea5c91b15d4187d9b234a
AsadShahzad1 <35929059+AsadShahzad1@users.noreply.github.com>
2018-02-13 01:56:13 +0500

    Add files via upload

---
+++ Lab 2/Task 2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        char character;
+        Console.Write("Enter an alphabet: ");
+        character = Char.Parse(Console.ReadLine());
+
+       
+            if (character == 'a' || character == 'A' || character == 'e' || character == 'E' || character == 'i'
+                || character == 'I' || character == 'o' || character == 'O' || character == 'u' || character == 'U')
+            {
+                Console.WriteLine(character + " is a vowel");
+            }
+           
+        else
+        {
+            Console.WriteLine(character + " is not an alphabet");
+        }
+            Console.ReadLine();
+    }
+}
